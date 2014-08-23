652eaf9b10514fa8cd489261355347f3f6491196
Mirko Günther <renegart@gmx.de>
2014-08-23 10:17:31 +0200

    change '/etc/mdev.conf' only if not already done

--- board/parallella/zynq7020/scripts/set_epiphany_device_rights.sh
+++ board/parallella/zynq7020/scripts/set_epiphany_device_rights.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 TARGETDIR=$1
 
-echo -e "\n# epiphany device" >> $TARGETDIR/etc/mdev.conf
-echo "epiphany	root:root 666" >> $TARGETDIR/etc/mdev.conf
+if ! ( grep -Fxq "epiphany	root:root 666" $TARGETDIR/etc/mdev.conf ) then
+  echo -e "\n# epiphany device" >> $TARGETDIR/etc/mdev.conf
+  echo "epiphany	root:root 666" >> $TARGETDIR/etc/mdev.conf
+fi
